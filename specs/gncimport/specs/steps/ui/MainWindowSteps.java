aa8ed86153f1e6b204b49f87423b771271acf00a
fernando <fernando@f454ceb6-da30-0410-8e11-d69e2d79837d>
2015-10-18 11:58:13 +0000

    synch'ed UI steps with new step design
    git-svn-id: svn://masaka/src/PersonalFinances/GncImport/trunk@1576 f454ceb6-da30-0410-8e11-d69e2d79837d

--- specs/gncimport/specs/steps/ui/MainWindowSteps.java
+++ specs/gncimport/specs/steps/ui/MainWindowSteps.java
@@ -12,85 +12,122 @@ import org.fest.swing.core.BasicRobot;
 import org.fest.swing.core.Robot;
 import org.fest.swing.edt.FailOnThreadViolationRepaintManager;
 
+import cucumber.api.DataTable;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import cucumber.runtime.PendingException;
 
-public class MainWindowSteps 
+public class MainWindowSteps
 {
 	private Robot robot;
 	private GncImportAppDriver _app;
 	private FileSystemDriver _fs;
+	private String _csvFileName;
 
 	@Before
 	public void beforeScenario()
 	{
-	    FailOnThreadViolationRepaintManager.install();
-	    robot = BasicRobot.robotWithNewAwtHierarchy();
-	    
-		_fs = new FileSystemDriver();
-		_fs.prepareTestFiles();	
+		FailOnThreadViolationRepaintManager.install();
+		robot = BasicRobot.robotWithNewAwtHierarchy();
 
-		_app = new GncImportAppDriver(robot);		
+		_fs = new FileSystemDriver();
+		_fs.prepareTestFiles();
+
+		_app = new GncImportAppDriver(robot);
 	}
-	
+
 	@After
 	public void afterScenario()
 	{
 		_app.shutdown();
 		_app = null;
-	    robot.cleanUp();
+		robot.cleanUp();
 	}
 	
-	@Given("^the sample CSV file \"([^\"]*)\" has been loaded$")
-	public void the_sample_csv_file_has_been_loaded(String fileName) throws Throwable 
+	@Given("^transactions have been exported into \"([^\"]*)\"$")
+	public void transactions_have_been_exported_into(String csvFileName) throws Throwable
 	{
-		_app.openCsvFile(TestFiles.CSV_1_TEST_FILE);
+		_csvFileName = TestFiles.getFilePath(csvFileName);
+	}
+
+	@When("^the transaction file is loaded$")
+	public void the_transaction_file_is_loaded() throws Throwable
+	{
+		_app.openCsvFile(_csvFileName);
 	}
 
 	@Then("^the app displays (\\d+) transactions$")
-	public void the_app_displays_transactions(int txCount) throws Throwable 
+	public void the_app_displays_transactions(int txCount) throws Throwable
 	{
 		_app.shouldDisplayTransactionCountInStatusBar(txCount);
 	}
 
 	@Then("^the transction grid shows (\\d+) rows$")
-	public void the_transction_grid_shows_rows(int txCount) throws Throwable 
+	public void the_transction_grid_shows_rows(int txCount) throws Throwable
 	{
 		_app.shouldDisplayTransactionGridWithTransactionCount(txCount);
 	}
-	
+
 	@Then("^displayed transactions match those in loaded file$")
-	public void displayed_transactions_match_those_in_loaded_file() throws Throwable 
+	public void displayed_transactions_match_those_in_loaded_file() throws Throwable
 	{
 		RbcExportParser parser = new RbcExportParser(TestFiles.CSV_1_TEST_FILE);
 		List<TxData> list = parser.getTransactions();
-		
+
 		_app.shouldDisplayTransactions(list);
 	}
-	
-	@Given("^the sample GNC file \"([^\"]*)\" has been loaded$")
-	public void the_sample_GNC_file_has_been_loaded(String arg1) throws Throwable 
+
+	@Given("^accounting data file \"([^\"]*)\" with default account \"([^\"]*)\"$")
+	public void accounting_data_file_with_default_account(String arg1, String arg2) throws Throwable
 	{
-	    // Express the Regexp above with the code you wish you had
-	    throw new PendingException();
+		// Express the Regexp above with the code you wish you had
+		throw new PendingException();
 	}
 
-	@Given("^the target account hierarchy has been set to \"([^\"]*)\"$")
-	public void the_target_account_hierarchy_has_been_set_to(String arg1) throws Throwable 
+	@When("^the accounting file is loaded$")
+	public void the_accounting_file_is_loaded() throws Throwable
 	{
-	    // Express the Regexp above with the code you wish you had
-	    throw new PendingException();
+		// Express the Regexp above with the code you wish you had
+		throw new PendingException();
+	}
+
+	@When("^the target account hierarchy is set to \"([^\"]*)\"$")
+	public void the_target_account_hierarchy_is_set_to(String arg1) throws Throwable
+	{
+		// Express the Regexp above with the code you wish you had
+		throw new PendingException();
 	}
 
 	@Then("^all transactions are associated with \"([^\"]*)\"$")
-	public void all_transactions_are_associated_with(String arg1) throws Throwable 
+	public void all_transactions_are_associated_with(String arg1) throws Throwable
 	{
-	    // Express the Regexp above with the code you wish you had
-	    throw new PendingException();
+		// Express the Regexp above with the code you wish you had
+		throw new PendingException();
 	}
 
+	@Given("^the following account override rules have been defined:$")
+	public void the_following_account_override_rules_have_been_defined(DataTable arg1) throws Throwable
+	{
+		// Express the Regexp above with the code you wish you had
+		// For automatic conversion, change DataTable to List<YourType>
+		throw new PendingException();
+	}
+
+	@Then("^all transactions matching \"([^\"]*)\" are associated with the account \"([^\"]*)\"$")
+	public void all_transactions_matching_are_associated_with_the_account(String arg1, String arg2) throws Throwable
+	{
+		// Express the Regexp above with the code you wish you had
+		throw new PendingException();
+	}
+
+	@Then("^all other transactions are associated with \"([^\"]*)\"$")
+	public void all_other_transactions_are_associated_with(String arg1) throws Throwable
+	{
+		// Express the Regexp above with the code you wish you had
+		throw new PendingException();
+	}
 
 }
