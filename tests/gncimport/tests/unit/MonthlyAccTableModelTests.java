412c5753fa18c887715db6104d54b48445e349e2
Fernando Cuenca <fernando_cuenca@yahoo.com.ar>
2017-02-07 16:53:02 -0500

    cleaned up additional duplication in tests

--- tests/gncimport/tests/unit/MonthlyAccTableModelTests.java
+++ tests/gncimport/tests/unit/MonthlyAccTableModelTests.java
@@ -1,6 +1,6 @@
 package gncimport.tests.unit;
 
-import static org.hamcrest.core.Is.is;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
@@ -52,7 +52,7 @@ public class MonthlyAccTableModelTests
 	@Test
 	public void displays_account_names_in_order()
 	{
-		assertTableModelDisplays("Misc Expenses", "Groceries", "Living Expenses");
+		assertTableModelDisplaysAccounts("Misc Expenses", "Groceries", "Living Expenses");
 	}
 	
 	@Test(expected=IllegalArgumentException.class)
@@ -81,13 +81,13 @@ public class MonthlyAccTableModelTests
 	}
 
 	@Test
-	public void updates_rule_definitions()
+	public void updating_accoung_only_changes_the_name()
 	{		
 		_tableModel.setValueAt("some other account", 0, IRRELEVANT);
-		assertTableModelDisplays("some other account", "Groceries", "Living Expenses");
+		assertTableModelDisplaysAccounts("some other account", "Groceries", "Living Expenses");
 		
 		ScreenValue newValue = (ScreenValue) _tableModel.getValueAt(0, IRRELEVANT);		
-		assertThat(newValue.domainValue(), is((Object)new MonthlyAccount(1, "some other account")));		
+		assertThat(((MonthlyAccount)newValue.domainValue()).sequenceNo, is(1));		
 	}
 	
 	@Test
@@ -113,25 +113,26 @@ public class MonthlyAccTableModelTests
 		
 		ScreenValue newValue = (ScreenValue) _tableModel.getValueAt(3, IRRELEVANT);
 		assertThat(newValue, is((ScreenValue)new ScreenValueForTest("", false)));
-		assertThat(newValue.domainValue(), is((Object)new MonthlyAccount(4, "")));
+		assertThat(((MonthlyAccount)newValue.domainValue()).sequenceNo, is(4));
 	}
 	
 	@Test
 	public void adding_account_notifies_listeners()
 	{
-		TableModelListener listener = mock(TableModelListener.class);
-		_tableModel.addTableModelListener(listener);
-		
-		_tableModel.newRow();
-		
-		verify(listener).tableChanged(any(TableModelEvent.class));
+		assertActionNotifiesListeners(new Runnable (){
+			@Override
+			public void run()
+			{
+				_tableModel.newRow();				
+			}
+		});
 	}
 
 	@Test
 	public void rules_can_be_removed()
 	{
 		_tableModel.removeRow(1);		
-		assertTableModelDisplays("Misc Expenses", "Living Expenses");
+		assertTableModelDisplaysAccounts("Misc Expenses", "Living Expenses");
 	}
 
 	@Test
@@ -139,41 +140,42 @@ public class MonthlyAccTableModelTests
 	{
 		_tableModel.removeRow(-1);
 		_tableModel.removeRow(3);
-		assertTableModelDisplays("Misc Expenses", "Groceries", "Living Expenses");
+		assertTableModelDisplaysAccounts("Misc Expenses", "Groceries", "Living Expenses");
 	}
 
 	@Test
 	public void removing_rule_notifies_listeners()
 	{
-		TableModelListener listener = mock(TableModelListener.class);
-		_tableModel.addTableModelListener(listener);
-		
-		_tableModel.removeRow(0);
-		
-		verify(listener).tableChanged(any(TableModelEvent.class));
+		assertActionNotifiesListeners(new Runnable()
+		{
+			public void run()
+			{
+				_tableModel.removeRow(0);
+			}
+		});
 	}
 	
 	@Test
 	public void accounts_are_renumbered_after_removal()
 	{
 		_tableModel.removeRow(1);
-		
-		assertThat(_accList.get(0).sequenceNo, is(1));
-		assertThat(_accList.get(1).sequenceNo, is(2));
+		assertAccountsAreInSequence();
 	}
-	
+
 	@Test
 	public void accounts_can_be_moved_up()
 	{
 		_tableModel.moveUp(1);
 		
-		assertTableModelDisplays("Groceries", "Misc Expenses", "Living Expenses");
+		assertTableModelDisplaysAccounts("Groceries", "Misc Expenses", "Living Expenses");
 	}
 
 	@Test
 	public void accounts_are_renumbered_after_moving_up()
 	{
 		_tableModel.moveUp(1);
+		
+		assertAccountsAreInSequence();
 		
 		assertThat(_accList.get(0).sequenceNo, is(1));
 		assertThat(_accList.get(1).sequenceNo, is(2));
@@ -183,32 +185,32 @@ public class MonthlyAccTableModelTests
 	public void moving_up_first_account_has_no_effect()
 	{
 		_tableModel.moveUp(0);
-		assertTableModelDisplays("Misc Expenses", "Groceries", "Living Expenses");
+		assertTableModelDisplaysAccounts("Misc Expenses", "Groceries", "Living Expenses");
 	}
 
 	@Test
 	public void out_of_bounds_selection_ignored_when_moving_up()
 	{
 		_tableModel.moveUp(-1);
-		assertTableModelDisplays("Misc Expenses", "Groceries", "Living Expenses");
+		assertTableModelDisplaysAccounts("Misc Expenses", "Groceries", "Living Expenses");
 
 		_tableModel.moveUp(_accList.size());
-		assertTableModelDisplays("Misc Expenses", "Groceries", "Living Expenses");
+		assertTableModelDisplaysAccounts("Misc Expenses", "Groceries", "Living Expenses");
 	}
 	
 	@Test
 	public void moving_up_notifies_listeners()
 	{
-		TableModelListener listener = mock(TableModelListener.class);
-		_tableModel.addTableModelListener(listener);
-		
-		_tableModel.moveUp(1);
-		
-		verify(listener).tableChanged(any(TableModelEvent.class));
+		assertActionNotifiesListeners(new Runnable()
+		{
+			public void run()
+			{
+				_tableModel.moveUp(1);
+			}
+		});
 	}
-
 	
-	private void assertTableModelDisplays(String... expectedAccNames)
+	private void assertTableModelDisplaysAccounts(String... expectedAccNames)
 	{
 		assertThat(_tableModel.getRowCount(), is(expectedAccNames.length));
 		
@@ -217,5 +219,32 @@ public class MonthlyAccTableModelTests
 			assertThat("mismatch at element: " + i,
 					_tableModel.getValueAt(i, IRRELEVANT), is((Object)(new ScreenValueForTest(expectedAccNames[i]))));			
 		}
+	}
+	
+	private void assertAccountsAreInSequence()
+	{
+		int lastSeqNo = Integer.MIN_VALUE;
+		for (int i = 0; i < _tableModel.getRowCount(); i++)
+		{
+			ScreenValue value = (ScreenValue) _tableModel.getValueAt(i, IRRELEVANT);	
+			MonthlyAccount acc = (MonthlyAccount) value.domainValue();
+			
+			if (lastSeqNo != Integer.MIN_VALUE)
+			{
+				assertThat("element: " + i + " is out of sequence",
+						acc.sequenceNo, is(lastSeqNo + 1));
+			}
+			lastSeqNo = acc.sequenceNo;
+		}
+	}
+
+	private void assertActionNotifiesListeners(Runnable action)
+	{
+		TableModelListener listener = mock(TableModelListener.class);
+		_tableModel.addTableModelListener(listener);
+				
+		action.run();
+		
+		verify(listener).tableChanged(any(TableModelEvent.class));
 	}
 }
